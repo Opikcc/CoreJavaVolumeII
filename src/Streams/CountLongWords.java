4f8d6ef640ca92931f29312fb7b69d5d05da04ea
Taufik <m_taufik@ecquaria.com>
2023-02-12 07:12:11 +0700

    Unicode Sample

---
+++ src/Streams/CountLongWords.java
@@ -0,0 +1,30 @@
+package Streams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ *
+ * @author Opick
+ */
+public class CountLongWords {
+  
+  public static void main(String[] args) throws IOException {
+    var contents = Files.readString(Paths.get("alice30.txt"));
+    List<String> words = List.of(contents.split("\\PL+"));
+    
+    long count = 0;
+    for (String w : words) {
+      if (w.length() > 12) count++;
+    }
+    System.out.println(count);
+    
+    count = words.stream().filter(w -> w.length() > 12).count();
+    System.out.println(count);
+    
+    count = words.parallelStream().filter(w -> w.length() > 12).count();
+    System.out.println(count);
+  }
+}
